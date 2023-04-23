a4ac342af15593dc2459e2482545b39e4dba3953
Alan123alan <kmnj500@astrazeneca.net>
2023-04-22 22:16:17 -0600

    testing bubletea for better TUI

--- go.mod
+++ go.mod
@@ -3,16 +3,34 @@ module github.com/Alan123alan/tasker
 go 1.17
 
 require (
+	github.com/fatih/color v1.15.0
+	github.com/rodaine/table v1.1.0
+	modernc.org/sqlite v1.21.0
+)
+
+require (
+	github.com/aymanbagabas/go-osc52 v1.2.1 // indirect
+	github.com/charmbracelet/bubbletea v0.23.2 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/fatih/color v1.15.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.14.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/rodaine/table v1.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.0.0-20201124115921-2c860bdd6e78 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	lukechampine.com/uint128 v1.2.0 // indirect
@@ -22,7 +40,6 @@ require (
 	modernc.org/mathutil v1.5.0 // indirect
 	modernc.org/memory v1.5.0 // indirect
 	modernc.org/opt v0.1.3 // indirect
-	modernc.org/sqlite v1.21.0 // indirect
 	modernc.org/strutil v1.1.3 // indirect
 	modernc.org/token v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,54 +1,135 @@
 package main
 
 import (
-	"database/sql"
-	"flag"
+	// "database/sql"
+	// "flag"
 	"fmt"
-	"log"
+	// "log"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	_ "modernc.org/sqlite"
 )
 
+type Model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
 func main() {
-	DB, err := sql.Open("sqlite", "./database.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer DB.Close()
-
-	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
-	getAll := getCmd.Bool("all", false, "Get all tasks")
-	getId := getCmd.String("id", "", "Get task by id")
-
-	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-	addDescription := addCmd.String("desc", "", "Task description")
-	addId := addCmd.String("id", "", "Task Id")
-
-	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
-	updateDescription := updateCmd.String("desc", "", "Task description")
-	updateStatus := updateCmd.Int("status", 0, "Task status")
-	updateId := updateCmd.String("id", "", "Task id")
-
-	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
-
-	if len(os.Args) < 2 {
-		fmt.Println("expected 'get', 'add', 'update' or 'create' commands")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
+	// DB, err := sql.Open("sqlite", "./database.db")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer DB.Close()
 
-	switch os.Args[1] {
-	case "get":
-		HandleGet(DB, getCmd, getAll, getId)
-	case "add":
-		HandleAdd(DB, addCmd, addId, addDescription)
-	case "update":
-		HandleUpdate(DB, updateCmd, updateId, updateDescription, (*Status)(updateStatus))
-	case "create":
-		HandleCreate(DB, createCmd)
-	case "help":
-	default:
-		fmt.Printf("'%v' is not a valid command. See './tasker -help'.", os.Args[1])
+	// getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	// getAll := getCmd.Bool("all", false, "Get all tasks")
+	// getId := getCmd.String("id", "", "Get task by id")
 
+	// addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	// addDescription := addCmd.String("desc", "", "Task description")
+	// addId := addCmd.String("id", "", "Task Id")
+
+	// updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	// updateDescription := updateCmd.String("desc", "", "Task description")
+	// updateStatus := updateCmd.Int("status", 0, "Task status")
+	// updateId := updateCmd.String("id", "", "Task id")
+
+	// createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+
+	// if len(os.Args) < 2 {
+	// 	fmt.Println("expected 'get', 'add', 'update' or 'create' commands")
+	// 	os.Exit(1)
+	// }
+
+	// switch os.Args[1] {
+	// case "get":
+	// 	HandleGet(DB, getCmd, getAll, getId)
+	// case "add":
+	// 	HandleAdd(DB, addCmd, addId, addDescription)
+	// case "update":
+	// 	HandleUpdate(DB, updateCmd, updateId, updateDescription, (*Status)(updateStatus))
+	// case "create":
+	// 	HandleCreate(DB, createCmd)
+	// case "help":
+	// default:
+	// 	fmt.Printf("'%v' is not a valid command. See './tasker -help'.", os.Args[1])
+
+	// }
+}
+
+func initialModel() Model {
+	return Model{
+		choices:  []string{"get", "add", "update", "create", "help"},
+		selected: make(map[int]struct{}),
 	}
+}
+
+func (m Model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m Model) View() string {
+	// The header
+	s := "Which operation do you want to perform?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
